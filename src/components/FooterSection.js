577244fefdbf0fbba7c6b340b0a64636656db23a
CapAgenda <capitalistagenda@pm.me>
2022-08-25 15:25:16 -0700

    changed emty href to # to try and resolve build error

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -1,7 +1,7 @@
 function FooterSection () {
     return (
     <section className="footer-container">
-        <div className="feed-links"><a href="">RSS</a> - <a href="">ATOM FEED</a> - <a href="">EMAIL</a></div>
+        <div className="feed-links"><a href="#">RSS</a> - <a href="#">ATOM FEED</a> - <a href="#">EMAIL</a></div>
         <div className="comic-text"><h4>Additional Content</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum laboriosam perferendis amet debitis quas. Tempora id ullam libero, totam, minima alias neque eveniet iure est architecto nobis sit inventore placeat?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum laboriosam perferendis amet debitis quas. Tempora id ullam libero, totam, minima alias neque eveniet iure est architecto nobis sit inventore placeat?</p>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,11 @@ function Nav() {
 <div className="nav-container">
     <nav>
         <ul className="nav-list">
-            <li><a href="">Link</a></li>
-            <li><a href="">Link</a></li>
-            <li><a href="">Link</a></li>
-            <li><a href="">Link</a></li>
-            <li><a href="">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
         </ul>
     </nav>
 </div>
