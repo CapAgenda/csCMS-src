ca7746caa8dc3a643417ce496de96a01dd45ce46
CapAgenda <capitalistagenda@pm.me>
2022-08-30 11:28:47 -0700

    test run to create json file- no image download 10

--- getimages.py
+++ getimages.py
@@ -11,7 +11,7 @@ comiclist = []
 
 # Loop through pages of feeds and add to list the title and url for each comic
 i=1
-while (i<=5):
+while (i<=1):
     urlpage = (allurls + str(i))
     f = feedparser.parse(urlpage)
     for feedentry in f.entries:
@@ -29,20 +29,20 @@ else:
 comic_json_list = []
 
 # initializing bad_chars_lists
-bad_chars = [';', ':', '!', "*", "?"]
+bad_chars = [';', ':', '!', "*", "?","\\"]
 bad_char_space = [" "]
+
+#Set images directory
+dir_path = 'src/images'
 
 #function that downloads a file and saves it 
 def download_image(location, file_name):
     #send GET request
     response = requests.get(location)
-    file_name = file_name
-    
-    #Set images directory
-    dir_path = 'src/images'
+        
     #Write image file
-    """ with open(os.path.join(dir_path, file_name), "wb") as f:
-            f.write(response.content) """
+    with open(os.path.join(dir_path, file_name), "wb") as f:
+            f.write(response.content)
  
 #download the files
 for item in comiclist:
@@ -52,7 +52,6 @@ for item in comiclist:
     header = h.headers
     content_type = header.get('content-type')
     extension = content_type.split('/')
-
     
     #Remove bad chars from file name
     file_name_title = ''.join(b for b in item[0] if not b in bad_chars)
@@ -61,11 +60,11 @@ for item in comiclist:
     #Add extension to file name
     file_name = file_name_clean +'.'+ extension[1]
     # Create the JSON list
-    json_list_item = {'Title':str(file_name_title), 'Ref':str("require('../images/")+str(file_name_clean)+"')"}
+    json_list_item = {'Title':str(file_name_title), 'Ref':str("require('../images/")+str(file_name)+"')"}
     comic_json_list.append(json_list_item)
     
     #Run download function  
-    download_image(location, str(file_name))
+   # download_image(location, str(file_name))
     
 # Save JSON list to repo
 jsonString = json.dumps(comic_json_list,  indent=4)
